16030d4ff43fe40ce9fdefd2de0a78f6a1f04763
r0qs <rodrigo.saramago@ethereum.org>
2023-11-27 14:53:59 -0300

    Introduce `Cancun` EVM version

--- .circleci/soltest_all.sh
+++ .circleci/soltest_all.sh
@@ -31,12 +31,9 @@ REPODIR="$(realpath "$(dirname "$0")"/..)"
 # shellcheck source=scripts/common.sh
 source "${REPODIR}/scripts/common.sh"
 
-DEFAULT_EVM_VALUES=(constantinople petersburg istanbul berlin london paris shanghai cancun prague)
-# Deserialize the EVM_VALUES array if it was provided as argument or
-# set EVM_VALUES to the default values.
-IFS=" " read -ra EVM_VALUES <<< "${1:-${DEFAULT_EVM_VALUES[@]}}"
-
-DEFAULT_EVM=cancun
+EVM_VALUES=(homestead byzantium constantinople petersburg istanbul berlin london paris shanghai cancun)
+DEFAULT_EVM=shanghai
+[[ " ${EVM_VALUES[*]} " =~ $DEFAULT_EVM ]]
 OPTIMIZE_VALUES=(0 1)
 
 # Run for ABI encoder v1, without SMTChecker tests.
